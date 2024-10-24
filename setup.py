17d3c79bd44f7eea75bca111f8a19e8fb9763428
kishtarn555 <hec_montemoon@hotmail.com>
2024-10-24 15:07:15 -0600

    [Release] 1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="karel",
-    version="1.2",
+    version="1.3",
     packages=find_packages(),
     entry_points={
         "cms.grading.languages": [
